842974a28699526863d009836536c34a87687401
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-02-14 18:13:59 +0000

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,29 +1,29 @@
 const Discord = require('discord.js');
 
-exports.run = async (client, message, args, tools) => {
+exports.run = (client, message, args, tools) => {
     
-    // Array of responses
-    let responses = [
-        'Aprovado!',
-        'Claro!',
-        'Nope.',
-        'Não',
-        'Talvez',
-        'Um dia...',
-        'Não vi isso acontecer!',
-        'Não tenho ideia'
-    ]
+  // Array of responses
+  let responses = [
+    'Aprovado!',
+    'Claro!',
+    'Nope.',
+    'Não',
+    'Talvez',
+    'Um dia...',
+    'Não vi isso acontecer!',
+    'Não tenho ideia'
+  ]
     
-    // Fetch a random item from the array
-    let fetched = responses[Math.floor(Math.random() * responses.length)];
+  // Fetch a random item from the array
+  let fetched = responses[Math.floor(Math.random() * responses.length)];
     
-    // Form Embed
-    const embed = new Discord.MessageEmbed()
-        .setColor(0xffffff)
-        .setFooter(fetched);
+  // Form Embed
+  const embed = new Discord.MessageEmbed()
+    .setColor(0xffffff)
+    .setFooter(fetched);
     
-    // Send Embed
-    message.channel.send(embed);
+  // Send Embed
+  message.channel.send(embed);
     
 }
 
